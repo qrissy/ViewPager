6b7eada4effe77dd74073c3cf99b07a9cd1f4979
qrissy <qris09@gmail.com>
2021-05-14 22:38:25 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/viewpager/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.viewpager;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        // Required empty public constructor
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new MondayFragment();
+        } else if (position == 1){
+            return new TuesdayFragment();
+        } else if (position == 2){
+            return new WednesdayFragment();
+        } else if (position == 3){
+            return new ThursdayFragment();
+        } else {
+            return new FridayFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+}
